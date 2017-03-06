1d0c9abd2ac33bdb1b5a18ae644a07d92b014797
Jay Chetty <jaypaulchetty@gmail.com>
2017-03-06 19:55:28 +0000

    regression single variable

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -7,14 +7,18 @@ function J = computeCost(X, y, theta)
 m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
-J = 0;
+% J = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
+predicted_values = X * theta;
 
+differences = predicted_values - y;
+squared_differences = differences.^2;
 
+J = sum(squared_differences) / (2*m);
 
 
 % =========================================================================
